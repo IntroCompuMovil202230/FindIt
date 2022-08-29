8c039c5c24eb1dc65167f0928623294bd19aca93
javier <javierm.ramirezp@gmail.com>
2022-08-28 22:30:44 -0500

    list view edit

--- app/src/main/java/com/ntn/findtit/MainActivity.kt
+++ app/src/main/java/com/ntn/findtit/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.ntn.findtit
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val b = findViewById<Button>(R.id.boton)
+        b.setOnClickListener { view ->
+            val intent = Intent(view!!.context, MisDesafios::class.java)
+            startActivity(intent)
+        }
+
+
     }
+
 }
