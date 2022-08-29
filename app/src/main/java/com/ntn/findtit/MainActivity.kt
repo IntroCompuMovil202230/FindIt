9fa65528ea9d14cebb55600dfc6a556a70eb12df
Zayber <zayber.re@gmail.com>
2022-08-28 22:17:06 -0500

    Frame tests

--- app/src/main/java/com/ntn/findtit/MainActivity.kt
+++ app/src/main/java/com/ntn/findtit/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.ntn.findtit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val t:Button = findViewById(R.id.button)
+        t.text = "TEst"
+        t.setOnClickListener {
+            val intent = Intent(this, PrincipalActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
