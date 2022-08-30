a9611cc6f55d71f32ecb980245c82d66eeb58e44
Zayber <zayber.re@gmail.com>
2022-08-29 19:01:13 -0500

    Fragment Account Added

--- app/src/main/java/com/ntn/findtit/ui/account/AccountFragment.kt
+++ app/src/main/java/com/ntn/findtit/ui/account/AccountFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.ntn.findtit.databinding.FragmentAccountBinding
@@ -18,18 +17,9 @@ class AccountFragment :Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val homeViewModel =
-            ViewModelProvider(this).get(AccountViewModel::class.java)
-
+    ): View {
         _binding = FragmentAccountBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textAccount
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return binding.root
     }
     override fun onDestroyView() {
         super.onDestroyView()
