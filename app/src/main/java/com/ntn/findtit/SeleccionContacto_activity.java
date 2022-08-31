1a4a9648b440d7e660f3bfb113b22037fa6e6e30
richfon <58142219+richfon@users.noreply.github.com>
2022-08-31 01:05:45 -0500

    registro,inicio y chat

---
+++ app/src/main/java/com/ntn/findtit/SeleccionContacto_activity.java
@@ -0,0 +1,14 @@
+package com.ntn.findtit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class seleccionContacto_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seleccion_contacto);
+    }
+}
