e21616be8156bb5c2e6dfa28bbe273fc4f8cf04c
javier <javierm.ramirezp@gmail.com>
2022-08-30 22:40:30 -0500

    create challenge

--- app/src/main/java/com/ntn/findtit/ui/mychallenge/Basic_data_challengeFragment.kt
+++ app/src/main/java/com/ntn/findtit/ui/mychallenge/Basic_data_challengeFragment.kt
@@ -5,15 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.ntn.findtit.R
 import com.ntn.findtit.adapters.DesafioEditarAdapter
+import com.ntn.findtit.databinding.FragmentBasicDataChallengeBinding
 import com.ntn.findtit.databinding.FragmentMyChallengesBinding
 import com.ntn.findtit.entity.Desafio
-import com.ntn.findtit.ui.challenge.ChallengeSearchFragment
 
-class MyChallengeFragment : Fragment() {
 
-    private var _binding: FragmentMyChallengesBinding? = null
+/**
+ * A simple [Fragment] subclass.
+ * Use the [Basic_data_challengeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class Basic_data_challengeFragment : Fragment() {
+
+    private var _binding: FragmentBasicDataChallengeBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,23 +31,14 @@ class MyChallengeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentMyChallengesBinding.inflate(inflater, container, false)
+        _binding = FragmentBasicDataChallengeBinding.inflate(inflater, container, false)
 
-        binding.createChallengeButton.setOnClickListener(object : View.OnClickListener {
+        binding.nextCreateBasic.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
-                val search = Basic_data_challengeFragment()
+                val search = Objetive_ChallengeFragment()
                 replaceFragment(search)
             }
         })
-
-        val desafios = ArrayList<Desafio>()
-        for(i in 0..25) {
-            val de = Desafio("numero"+i, "na", i, i,5);
-            desafios.add(de);
-        }
-        val d = DesafioEditarAdapter(context, desafios);
-        val list = binding.lista
-        list.adapter = d
 
         return binding.root
     }
